2ab2867719e04472040f3f149ea52a6d7d7db6ed
Fadi Asaad <firon1222@gmail.com>
2023-09-30 04:21:44 +0300

    Fix runtime errors and implement PUT method request to update State object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,7 @@ view for State objects
 from api.v1.views import app_views
 from models.state import State
 from models import storage
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -41,8 +41,8 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
     """Create a State."""
-    new_state = request.get_json()
-    if not new_state:
+    new_state = request.get_json(silent=True)
+    if new_state is None:
         abort(400, 'Not a JSON')
     if 'name' not in new_state.keys():
         abort(400, 'Missing name')
@@ -50,3 +50,24 @@ def post_state():
     storage.new(new_state_ins)
     storage.save()
     return make_response(jsonify(new_state_ins.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state_by_id(state_id):
+    """Update a State object by state_id"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    # get attributes from request body
+    attrs = request.get_json(silent=True)
+    if attrs is None:
+        abort(400, 'Not a JSON')
+
+    for key, value in attrs.items():
+        # if key is not a system attribute then update the field with key
+        if key not in {'id', 'updated_at', 'created_at'}:
+            setattr(state, key, value)
+
+    storage.save()
+    return jsonify(state.to_dict())
